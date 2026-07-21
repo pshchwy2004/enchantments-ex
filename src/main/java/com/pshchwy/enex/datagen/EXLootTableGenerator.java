d41fe415e15bd10f6b93f94a6478f6e5337bd878
BiiPii <creeperguy908@gmail.com>
2026-07-21 18:48:57 -0400

    Added data generator for loot table

---
+++ src/main/java/com/pshchwy/enex/datagen/EXLootTableGenerator.java
@@ -0,0 +1,19 @@
+package com.pshchwy.enex.datagen;
+
+import com.pshchwy.enex.block.EXBlocks;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
+import net.minecraft.core.HolderLookup;
+
+import java.util.concurrent.CompletableFuture;
+
+public class EXLootTableGenerator extends FabricBlockLootTableProvider {
+    public EXLootTableGenerator(FabricDataOutput dataOutput, CompletableFuture<HolderLookup.Provider> registryLookup) {
+        super(dataOutput, registryLookup);
+    }
+
+    @Override
+    public void generate() {
+        dropSelf(EXBlocks.STAMPING_TABLE);
+    }
+}
